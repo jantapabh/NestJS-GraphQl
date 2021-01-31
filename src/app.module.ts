2ea39b9b9f4dcffc039f901e0e57ba3dd6f3286f
jantapabh <s6035512034@phuket.psu.ac.th>
2021-01-31 23:18:08 +0700

    add commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql'
-// import { BookModule } from './book/book.module';
-import { BookResolver } from './book/book.resolver';
 import { CatsModule } from './cats/cats.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsResolver } from './cats/cats.resolver';
--- src/book/book.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BookResolver } from './book.resolver';
-
-@Module({
-  providers: [BookResolver]
-})
-export class BookModule {}
